5e67f2d8c84e8fcb506fd6f55b5a89c6e9bbc2b2
KHALYBOSS <khalyboss198@gmail.com>
2024-03-28 23:09:42 +0400

    added lessons

---
+++ lesson.js
@@ -0,0 +1,83 @@
+const LESSONS = [
+    {
+      id: 1,
+      subject: 'Math',
+      location: 'Germany',
+      price: 20,
+      spaces: 5,
+      icon: 'fas fa-calculator'
+    },
+    {
+      id: 2,
+      subject: 'Physics',
+      location: 'United States',
+      price: 25,
+      spaces: 3,
+      icon: 'fas fa-atom'
+    },
+    {
+      id: 3,
+      subject: 'History',
+      location: 'France',
+      price: 15,
+      spaces: 8,
+      icon: 'fas fa-book'
+    },
+    {
+      id: 4,
+      subject: 'English',
+      location: 'United Kingdom',
+      price: 18,
+      spaces: 6,
+      icon: 'fas fa-language'
+    },
+    {
+      id: 5,
+      subject: 'Biology',
+      location: 'Canada',
+      price: 22,
+      spaces: 4,
+      icon: 'fas fa-microscope'
+    },
+    {
+      id: 6,
+      subject: 'Chemistry',
+      location: 'Australia',
+      price: 30,
+      spaces: 2,
+      icon: 'fas fa-flask'
+    },
+    {
+      id: 7,
+      subject: 'Computer Science',
+      location: 'Japan',
+      price: 28,
+      spaces: 5,
+      icon: 'fas fa-laptop-code'
+    },
+    {
+      id: 8,
+      subject: 'Art',
+      location: 'Italy',
+      price: 20,
+      spaces: 7,
+      icon: 'fas fa-paint-brush'
+    },
+    {
+      id: 9,
+      subject: 'Music',
+      location: 'Spain',
+      price: 25,
+      spaces: 4,
+      icon: 'fas fa-music'
+    },
+    {
+      id: 10,
+      subject: 'Physical Education',
+      location: 'Brazil',
+      price: 15,
+      spaces: 10,
+      icon: 'fas fa-dumbbell'
+    }
+  ];
+  
